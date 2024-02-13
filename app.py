05125e3dba50b60c6ea84732aa83c056c8f741c2
Clara Battesini <clarabattesini@gmail.com>
2024-02-12 20:19:18 -0500

    feat: Installing Streamlit and making a simple page

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.write("# Pet Food Analysis")
+
+st.write(
+    "> **Objective:** As a new business owner of pet food subscription service, I want to understand the pet food market and the preferences of pet owners so that I can choose the right products for my business."
+)
